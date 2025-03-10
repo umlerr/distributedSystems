d01211aa1fb090fa0c201139cd40a8da0413be6c
umlerr <ugrumov.misha@yandex.ru>
2025-03-10 20:49:04 +0300

    Фиксы для микросервиса авторизации

---
+++ microservices/service-auth/src/main/java/umlerr/serviceauth/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package umlerr.serviceauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Primary
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ microservices/service-auth/src/main/java/umlerr/serviceauth/dto/AuthDTO.java
@@ -0,0 +1,13 @@
+package umlerr.serviceauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class AuthDTO {
+    private String email;
+    private String password;
+}
